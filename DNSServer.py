b56d31f50a6737d08640cf7bdafb9601fd3011d8
abz2032-cpu <abz2032@nyu.edu>
2025-10-25 19:08:37 +0200

    Update DNSServer.py

--- DNSServer.py
+++ DNSServer.py
@@ -5,7 +5,6 @@ import dns.rdtypes
 import dns.rdtypes.ANY
 from dns.rdtypes.ANY.MX import MX
 from dns.rdtypes.ANY.SOA import SOA
-from dns import name as _dn
 import dns.rdata
 import dns.rrset
 import socket
@@ -13,7 +12,6 @@ import threading
 import signal
 import os
 import sys
-import dns.rcode
 
 import hashlib
 from cryptography.fernet import Fernet
@@ -43,8 +41,6 @@ def encrypt_with_aes(input_string, password, salt):
 def decrypt_with_aes(encrypted_data, password, salt):
     key = generate_aes_key(password,salt)
     f = Fernet(key)
-    if isinstance(encrypted_data, str):          # accept token as str or bytes
-        encrypted_data = encrypted_data.encode('ascii')
     decrypted_data = f.decrypt(encrypted_data) #call the Fernet decrypt method
     return decrypted_data.decode('utf-8')
 
@@ -105,7 +101,7 @@ dns_records = {
 
 def run_dns_server():
     # Create a UDP socket and bind it to the local IP address (what unique IP address is used here, similar to webserver lab) and port (the standard port for DNS)
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Research this
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Research this
     server_socket.bind(('127.0.0.1', 53))
 
     while True:
@@ -126,63 +122,36 @@ def run_dns_server():
             if qname in dns_records and qtype in dns_records[qname]:
                 # Retrieve the data for the record and create an appropriate `rdata` object for it
                 answer_data = dns_records[qname][qtype]
+
                 rdata_list = []
 
                 if qtype == dns.rdatatype.MX:
                     for pref, server in answer_data:
-                        rdata_list.append(MX(
-                            dns.rdataclass.IN, dns.rdatatype.MX,
-                            int(pref), _dn.from_text(server)
-                        ))
-
+                        rdata_list.append(MX(dns.rdataclass.IN, dns.rdatatype.MX, pref, server))
                 elif qtype == dns.rdatatype.SOA:
-                    mname, rname, serial, refresh, retry, expire, minimum = answer_data
-                    rdata_list.append(SOA(
-                        dns.rdataclass.IN, dns.rdatatype.SOA,
-                        _dn.from_text(mname), _dn.from_text(rname),
-                        int(serial), int(refresh), int(retry), int(expire), int(minimum)
-                    ))
-
+                    mname, rname, serial, refresh, retry, expire, minimum = answer_data # What is the record format? See dns_records dictionary. Assume we handle @, Class, TTL elsewhere. Do some research on SOA Records
+                    rdata = SOA(dns.rdataclass.IN, dns.rdatatype.SOA, mname, rname, serial, refresh, retry, expire, minimum) # follow format from previous line
+                    rdata_list.append(rdata)
                 else:
-                    if qtype == dns.rdatatype.TXT:
-                        if isinstance(answer_data, str):
-                            rdata_list = [dns.rdata.from_text(dns.rdataclass.IN, qtype, f'"{answer_data}"')]
-                        else:
-                            rdata_list = [
-                                dns.rdata.from_text(dns.rdataclass.IN, qtype, f'"{s}"')
-                                for s in answer_data
-                            ]
+                    if isinstance(answer_data, str):
+                        rdata_list = [dns.rdata.from_text(dns.rdataclass.IN, qtype, answer_data)]
                     else:
-                        if isinstance(answer_data, str):
-                            rdata_list = [dns.rdata.from_text(dns.rdataclass.IN, qtype, answer_data)]
-                        else:
-                            rdata_list = [
-                                dns.rdata.from_text(dns.rdataclass.IN, qtype, s)
-                                for s in answer_data
-                            ]
-
-                # Build one RRset for qname/qtype and add all rdata
-                rrset = dns.rrset.RRset(question.name, dns.rdataclass.IN, qtype)
+                        rdata_list = [dns.rdata.from_text(dns.rdataclass.IN, qtype, data) for data in answer_data]
                 for rdata in rdata_list:
-                    rrset.add(rdata)
-                response.answer.append(rrset)
+                    response.answer.append(dns.rrset.RRset(question.name, dns.rdataclass.IN, qtype))
+                    response.answer[-1].add(rdata)
 
-                # Authoritative (AA) only when we answered from our table
-                response.flags |= 1 << 10
-
-            else:
-                # Name/type not found -> SERVFAIL
-                response.set_rcode(dns.rcode.SERVFAIL)
-                response.answer.clear()
-                response.flags &= ~ (1 << 10) 
+            # Set the response flags
+            response.flags |= 1 << 10
 
             # Send the response back to the client using the `server_socket.sendto` method and put the response to_wire(), return to the addr you received from
             print("Responding to request:", qname)
             server_socket.sendto(response.to_wire(), addr)
-        except KeyboardInterrupt:
+        except KeyboardInterrupt:           
             print('\nExiting...')
             server_socket.close()
             sys.exit(0)
+
 
 def run_dns_server_user():
     print("Input 'q' and hit 'enter' to quit")
